35db440866c7e880239796ed217f9cc622a81888
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-06 22:03:35 +0100

    finish the task

--- Hello.py
+++ Hello.py
@@ -11,4 +11,8 @@ cell = sheet.cell(1, 1)
 # Create a for loop
 for row in range(2, sheet.max_row + 1):
     cell = sheet.cell(row, 3)
-    print(cell.value)
+    corrected_price = cell.value * 0.9
+    corrected_price_cell = sheet.cell(row, 4)
+    corrected_price_cell.value = corrected_price
+
+wb.save('file2.xlsx')
